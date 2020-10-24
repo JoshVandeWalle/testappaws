f31a3d3b802ab065a1e70d5010e7ac638870cbc5
JoshVandeWalle <joshv98@icloud.com>
2020-10-24 00:55:24 -0700

    Update api.js

--- src/actions/api.js
+++ src/actions/api.js
@@ -1,14 +1,13 @@
 import axios from "axios";
-
-const baseUrl = "https://54.219.163.122:8080/";
-//const baseUrl = "http://localhost:8080/";
+const baseUrl = "http://54.219.163.122:8080/";
+//const baseUrl = "http://localhost:8080/service/getjson";
 
 export default {
     // functions for each controller in .NET CORE
-    comment(url = baseUrl+"comment/"){
+    comment(url = baseUrl+"service/"){
         return {
             // for comment service
-            fetchAll: () => axios.get(url),
+            fetchAll: () => axios.get(url+"/getjson"),
             create: (newRecord) => axios.post(url+"publish", newRecord),
             fetchById: (id) => axios.get(url+id),
             update: (updatedRecord) => axios.post(url+"edit", updatedRecord),
